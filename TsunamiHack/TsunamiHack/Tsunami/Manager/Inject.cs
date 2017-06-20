b13e38e4a503d0d60c8d0aca2a4256104eb691aa
Zach Taylor <zach@planetjester.com>
2017-06-20 16:48:58 -0400

    Changes

---
+++ TsunamiHack/TsunamiHack/Tsunami/Manager/Inject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TsunamiHack.Tsunami.Manager
+{
+    static class Install
+    {
+
+        static void Inject()
+        {
+            private static GameObject _hook;
+            private static 
+        }
+
+    }
+}
