075d6341e5fca34aeddaeb78307dda41e7d2d60f
Zach Taylor <zach@planetjester.com>
2017-06-23 02:41:28 -0400

    going to sleep

---
+++ TsunamiHack/TsunamiHack/Tsunami/Types/KeybindConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TsunamiHack.Tsunami.Types
+{
+    public class KeybindConfig
+    {
+        private Dictionary<string, KeyCode> KeyList;
+
+        public void addBind(string name, KeyCode key)
+        {
+            if (KeyList.ContainsKey(name) || KeyList.ContainsValue(key))
+            {
+                return;
+            }
+
+            KeyList.Add(name,key);
+        }
+    }
+}
