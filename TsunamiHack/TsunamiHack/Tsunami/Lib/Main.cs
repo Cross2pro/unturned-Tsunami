a5e90495696cd821f2aa07ff41adbb3f3aff46d5
Zach Taylor <zach@planetjester.com>
2017-08-05 20:14:49 -0400

    working to fix a recoil hack bug

--- TsunamiHack/TsunamiHack/Tsunami/Lib/Main.cs
+++ TsunamiHack/TsunamiHack/Tsunami/Lib/Main.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
 using SDG.Unturned;
 using TsunamiHack.Tsunami.Manager;
 using UnityEngine;
@@ -9,7 +9,6 @@ namespace TsunamiHack.Tsunami.Lib
 {
     internal class Main
     {
-        internal static Dictionary<string, ItemGunAsset> Backups = new Dictionary<string, ItemGunAsset>();
         internal static FieldInfo spreadAim;
         internal static FieldInfo spreadHip;
         internal static FieldInfo ballisticForce;
@@ -27,31 +26,47 @@ namespace TsunamiHack.Tsunami.Lib
             ballisticDrop = typeof(ItemGunAsset).GetField("_ballisticDrop", BindingFlags.Instance | BindingFlags.NonPublic);
         }
         
+        internal static List<ItemGunAsset> backuplist = new List<ItemGunAsset>(); 
+        
         public static void Update()
         {
+
             if (menu.NoRecoil && Player.player.equipment.asset is ItemGunAsset)
             {
-                if(!Backups.ContainsKey(Player.player.equipment.asset.itemName))
-                    Backups.Add(Player.player.equipment.asset.itemName, Player.player.equipment.asset as ItemGunAsset);
-                
                 var asset = Player.player.equipment.asset as ItemGunAsset;
+
+                if (!backuplist.Contains(asset))
+                {
+                    backuplist.Add(asset);
+                }
 
                 asset.recoilMax_x = 0f;
                 asset.recoilMax_y = 0f;
                 asset.recoilMin_x = 0f;
                 asset.recoilMin_y = 0f;
             }
-            if (!menu.NoRecoil && Backups.ContainsKey(Player.player.equipment.asset.itemName))
+            else
             {
-                var asset = Player.player.equipment.asset as ItemGunAsset;
-                var backupasset = Backups[Player.player.equipment.asset.itemName];
+                if (Player.player.equipment.asset is ItemGunAsset)
+                {
+                    if(Array.Exists(backuplist.ToArray(), asset => asset.item == (Player.player.equipment.asset as ItemGunAsset).item))
+                    {
+                        var gun = Array.Find(backuplist.ToArray(),
+                            asset => asset.item == (Player.player.equipment.asset as ItemGunAsset).item);
 
-                asset.recoilMax_x = backupasset.recoilMax_x;
-                asset.recoilMax_y = backupasset.recoilMax_y;
-                asset.recoilMin_x = backupasset.recoilMin_x;
-                asset.recoilMin_y = backupasset.recoilMin_y;
+                        if (gun != null)
+                        {
+                            var current = Player.player.equipment.asset as ItemGunAsset;
+
+                            current.recoilMax_x = gun.recoilMax_x;
+                            current.recoilMax_y = gun.recoilMax_y;
+                            current.recoilMin_x = gun.recoilMin_x;
+                            current.recoilMin_y = gun.recoilMin_y;
+                        }
+                    }
+                }
+                
             }
-            
             
         }
     }
---
+++ TsunamiHack/TsunamiHack/Tsunami/Types/GunAsset.cs
@@ -0,0 +1,9 @@
+﻿namespace TsunamiHack.Tsunami.Types
+{
+    internal class GunAsset
+    {
+        public float recoilminx;
+        public float recoilminy;
+        public float recoilmaxx;
+        public float recoilmaxy;}
+}
